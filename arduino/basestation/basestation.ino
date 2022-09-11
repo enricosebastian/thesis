b540dbfcad2a8abce0e15941ac89282a68bcc974
enricosebastian <enricosebastiansalazar@gmail.com>
2022-09-11 15:14:17 +0800

    Added more arduino files

---
+++ arduino/basestation/basestation.ino
@@ -0,0 +1,56 @@
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+#include <LinkedList.h>
+
+SoftwareSerial HC12(6, 7); // HC-12 TX Pin, HC-12 RX Pin
+
+StaticJsonDocument<200> sent;
+StaticJsonDocument<200> received;
+
+LinkedList<String> myDrones;
+
+String command = "";
+String message = "";
+
+String readCommand() {
+  DeserializationError err = deserializeJson(received, HC12);
+  if(err == DeserializationError::Ok) {
+    return received["COMMAND"].as<String>();;
+  } else {
+    Serial.print(err.c_str());
+    while(HC12.available() > 0) {
+      HC12.read();
+    }
+  }
+  return "ERROR";
+}
+
+bool hasReceivedMessage() {
+  if(HC12.available()) {
+    return true;
+  }
+  return false;
+}
+
+void setup() {
+  Serial.begin(9600);
+  HC12.begin(9600); 
+  
+  pinMode(10, OUTPUT); // DRONE 1
+  pinMode(11, OUTPUT); // DRONE 2
+  pinMode(12, OUTPUT); // DRONE 3
+
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+}
+
+void loop() {
+  if(hasReceivedMessage()) {
+    command = readCommand();
+    if(command == "HELLO") {
+      digitalWrite(10, HIGH);
+      delay(1000);
+    }
+  }
+  digitalWrite(10, LOW);
+}
