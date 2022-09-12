e05d4c37c0de08245c9ed7355a50ed23e52c51b2
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2022-09-12 14:52:13 +0800

    Added proper locations of files

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 . "/home/pi/Desktop/tf/bin/activate" #activate the python virtual environment
 cd /home/pi/Desktop/thesis/python
 
-# lxterminal -e python window-rename.py
 lxterminal -e python initial.py
 lxterminal -e python reader.py
-# lxterminal -e python thesis-receiver.py
+
+cd /home/pi/Desktop/thesis/detect
+lxterminal -e python detect.py
