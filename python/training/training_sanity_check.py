d747fd6846b9aae45af44973e28da8ab4d29921f
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2022-09-28 12:39:59 +0800

    Added new models trained using Pierre's dataset

---
+++ python/training/training_sanity_check.py
@@ -0,0 +1,18 @@
+# based on: https://www.youtube.com/watch?v=sarZ_FZfDxs
+
+import numpy as np
+import os
+
+from tflite_model_maker.config import ExportFormat
+from tflite_model_maker import model_spec
+from tflite_model_maker import object_detector
+
+import tensorflow as tf
+assert tf.__version__.startswith('2')
+
+tf.get_logger().setLevel('ERROR')
+from absl import logging
+logging.set_verbosity(logging.ERROR)
+
+print(tf.config.list_physical_devices('GPU'))
+spec = model_spec.get('efficientdet_lite0')
