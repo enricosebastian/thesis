82b089f59ae6c8f3384935883566544f2ea72c0e
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2022-10-19 11:09:10 +0800

    Added auto shutdown script

--- python/sending_json_demo.py
+++ python/sending_json_demo.py
@@ -1,6 +1,12 @@
 import serial
 import time
 import json
+import os
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(13, GPIO.IN)
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0)
 ser.flush()
@@ -15,20 +21,17 @@ for i in range(10):
     time.sleep(1)
     
 while True:
-    message = input()
-    if(message == "true"):
+    print(GPIO.input(13))
+    
+    if GPIO.input(13) == GPIO.HIGH:
         data = {}
         data["is_found"] = True
         data["origin_x"] = 300
         data = json.dumps(data)
         print(data)
         ser.write(data.encode('utf-8'))
-    else:
-        data = {}  
-        data["is_found"] = False
-        data = json.dumps(data)
-        print(data)
-        ser.write(data.encode('utf-8'))
+        os.system("shutdown now -h")
+        time.sleep(1)
         
 
 # for i in range(5):
