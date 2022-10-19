4511dbd001fad9ae46edfd42b5d827ec9f83b3f2
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2022-10-19 09:18:48 +0800

    Added demo.py

--- demo.sh
+++ demo.sh
@@ -3,4 +3,4 @@
 . "/home/pi/Desktop/tf/bin/activate" #activate the python virtual environment
 cd /home/pi/Desktop/thesis/python
 
-xterm -e python /home/pi/Desktop/thesis/python/detect.py
+xterm -e python /home/pi/Desktop/thesis/python/demo.py
