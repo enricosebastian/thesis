54e9afe522e62a5f59d7b2d195ad5bb8cf1d2c54
enricosebastian <enricosebastiansalazar@gmail.com>
2022-12-15 01:48:23 +0800

    Improved drone and base station demo

--- arduino/demo/drone_demo/drone_demo.ino
+++ arduino/demo/drone_demo/drone_demo.ino
@@ -1,28 +1,7 @@
-#include <Servo.h>
 #include <ArduinoJson.h>
 #include <SoftwareSerial.h>
-#include <Arduino.h>
-#include <Wire.h>
-#include <HMC5883L_Simple.h>
-
-HMC5883L_Simple Compass;
-/*
- * GY-273 Compass Module  ->  Arduino
- * VCC  -> VCC  (See Note Below)
- * GND  -> GND
- * SCL  -> A5/SCL, (Use Pin 21 on the Arduino Mega)
- * SDA  -> A4/SDA, (Use Pin 20 on the Arduino Mega)
- * DRDY -> Not Connected (in this example)
- */
-
- // if the angle increases, it's going to the right
- // if the angle decreases, it's heading to the left
- 
 
 SoftwareSerial HC12(6, 7); // HC-12 TX Pin, HC-12 RX Pin
-
-Servo escRight;
-Servo escLeft;
 
 StaticJsonDocument<200> sent;
 StaticJsonDocument<200> received;
@@ -34,20 +13,19 @@ const int greenLed = 12;
 bool isConnected = false;
 bool isDeployed = false;
 
-int motorSpeedLeft = 0;
-int motorSpeedRight = 0;
-int allowance = 5;
-int escThreshold = 20;
-int controllerDelay = 0;
-float mainHeading = 0;
-float heading = 0;
-String currentCommand = "";
-unsigned long duration = 0;
+//int motorSpeedLeft = 0;
+//int motorSpeedRight = 0;
+//int allowance = 5;
+//int escThreshold = 20;
+//int controllerDelay = 0;
+//float mainHeading = 0;
+//float heading = 0;
+//String currentCommand = "";
+//unsigned long duration = 0;
 
 void setup() {
   Serial.begin(9600);
   HC12.begin(9600);
-  Wire.begin(9600);
   Serial.println("Starting...");
 
   pinMode(greenLed, OUTPUT);
@@ -56,22 +34,22 @@ void setup() {
   digitalWrite(greenLed, LOW);
   digitalWrite(redLed, LOW);
   
-  Compass.SetDeclination(-2, 37, 'W');
-  Compass.SetSamplingMode(COMPASS_SINGLE);
-  Compass.SetScale(COMPASS_SCALE_130);
-  Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
-
-  escLeft.attach(10,1000,2000);
-  escRight.attach(11,1000,2000);
-  
-  escLeft.write(0);
-  escRight.write(0);
-  delay(2000);
+//  Compass.SetDeclination(-2, 37, 'W');
+//  Compass.SetSamplingMode(COMPASS_SINGLE);
+//  Compass.SetScale(COMPASS_SCALE_130);
+//  Compass.SetOrientation(COMPASS_HORIZONTAL_X_NORTH);
+//
+//  escLeft.attach(10,1000,2000);
+//  escRight.attach(11,1000,2000);
+//  
+//  escLeft.write(0);
+//  escRight.write(0);
+//  delay(2000);
   Serial.println("Initialization success. Looking for base station...");
 }
 
 void loop() {
-
+  
   //Task 1: Establish connection with base station
   while(!isConnected) {
     connectToBaseStation();
@@ -88,22 +66,26 @@ void loop() {
   }
 
   //Task 4: Deploy. Do stuff.
+  digitalWrite(greenLed, HIGH);
+  digitalWrite(redLed, LOW);
+  
   while(true) {
     Serial.println("Deployed!");
   }
 }
 
 void connectToBaseStation() {
-  Serial.println("Connecting to base station...");
-
   String sentCommand = "CONNECT";
   String sentToName = "BaseStation";
-  String sentDetails = "Connecting to base station...";
+  String sentDetails = "Connecting to BaseStation";
   bool sentResponse = true;
+
+  Serial.println(sentDetails);
   
   while(!sendCommand(sentCommand, sentToName, sentDetails, sentResponse)) {
     //Keep sending the command until it's a success
   }
+  
   isConnected = true;
 }
 
