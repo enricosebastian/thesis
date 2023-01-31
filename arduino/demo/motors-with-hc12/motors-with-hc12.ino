706dfde3d127fba9d1633029b924683721fcb17f
unknown <enrico_sebastian_salazar@dlsu.edu.ph>
2023-01-31 19:19:05 +0800

    removed too many serialprintlns

--- arduino/demo/motors-with-hc12/motors-with-hc12.ino
+++ arduino/demo/motors-with-hc12/motors-with-hc12.ino
@@ -50,7 +50,6 @@ void loop() {
 
   if(HC12.available()) {
     char letter = HC12.read();
-    Serial.println(letter);
     if(letter == '\0') {
       receivedMessage += '\0';
       Serial.print("received: ");
