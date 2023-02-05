5b50ad5bbc99b534adfe6cecd22041d01d08d7b9
enricosebastian <enricosebastiansalazar@gmail.com>
2023-02-05 23:23:55 +0800

    changed port number for nano

--- arduino/demo/nano-uno-hc12/nano-uno-hc12.ino
+++ arduino/demo/nano-uno-hc12/nano-uno-hc12.ino
@@ -3,14 +3,14 @@
 //Constants (buttons)
 const int txHc12 = A0; //green tx
 const int rxHc12 = A1; //blue received
-const int txNano = A2; //green tx
-const int rxNano = A3; //blue received
+const int txNano = 9; //green tx
+const int rxNano = 8; //blue received
 const int waitingTime = 5000;
 
 String message = "";
 
 NeoSWSerial HC12(txHc12, rxHc12); // (Green TX, Blue RX)
-// NeoSWSerial Nano(txNano, rxNano); // (Green TX, Blue RX)
+NeoSWSerial Nano(txNano, rxNano); // (Green TX, Blue RX)
 
 void setup() {
   Serial.begin(9600);
