6f93d8f6d60df183c4c479ae00d7965d3e064504
enricosebastian <enricosebastiansalazar@gmail.com>
2023-02-06 10:58:52 +0800

    added complexity to listening

--- arduino/demo/nano-uno-hc12-listening/nano-uno-hc12-listening.ino
+++ arduino/demo/nano-uno-hc12-listening/nano-uno-hc12-listening.ino
@@ -7,6 +7,9 @@ const int txNano = 9; //green tx
 const int rxNano = 8; //blue received
 const int waitingTime = 5000;
 
+bool isListeningToHc12 = true;
+bool isListeningToNano = false;
+
 String message = "";
 
 NeoSWSerial HC12(txHc12, rxHc12); // (Green TX, Blue RX)
@@ -15,16 +18,34 @@ NeoSWSerial Nano(txNano, rxNano); // (Green TX, Blue RX)
 void setup() {
   Serial.begin(9600);
   HC12.begin(9600);
-  // Nano.begin(9600);
+  Nano.begin(9600);
+  HC12.listen();
   Serial.println("Initializing...");
 }
 
 void loop() {
-
-  HC12.listen();
-  if(HC12.available()) {
+  if(!isListeningToNano && HC12.available()) {
     char letter = HC12.read();
     Serial.println(letter);
+  }
+
+  if(Serial.available()) {
+    char letter = Serial.read();
+    if(letter == "\n") {
+      message += "\n";
+      isListeningToNano = true;
+    } else {
+      message += letter;
+    }
+  }
+
+  if(isListeningToNano) {
+    HC12.end();
+    Nano.listen();
+    Nano.print(message);
+    Nano.end();
+    isListeningToNano = false;
+    HC12.listen();
   }
   
 }
