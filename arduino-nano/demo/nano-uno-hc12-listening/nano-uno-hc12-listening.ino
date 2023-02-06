dc4b4ea31a167cbcef7d345e86ee37369d36f7b4
enricosebastian <enricosebastiansalazar@gmail.com>
2023-02-06 11:28:34 +0800

    fixed nano issues

--- arduino-nano/demo/nano-uno-hc12-listening/nano-uno-hc12-listening.ino
+++ arduino-nano/demo/nano-uno-hc12-listening/nano-uno-hc12-listening.ino
@@ -1,59 +1,39 @@
 #include <NeoSWSerial.h>
 
-//Constants (buttons)
-const int txHc12 = A0; //green tx
-const int rxHc12 = A1; //blue received
-const int txNano = 9; //green tx
-const int rxNano = 8; //blue received
-const int waitingTime = 5000;
-
-bool isListeningToHc12 = true;
-bool isListeningToNano = false;
-bool isSendingToNano = false;
-
+const int redLed = 2;
 String message = "";
 
-NeoSWSerial HC12(txHc12, rxHc12); // (Green TX, Blue RX)
-NeoSWSerial Nano(txNano, rxNano); // (Green TX, Blue RX)
+const int txNano = 9;
+const int rxNano = 8;
+
+NeoSWSerial Nano(txNano, rxNano);
 
 void setup() {
   Serial.begin(9600);
-  HC12.begin(9600);
   Nano.begin(9600);
-  HC12.listen();
-  Serial.println("Initializing...");
+
+  Nano.listen();
+  pinMode(redLed, OUTPUT);
+  digitalWrite(redLed, LOW);
+  Serial.println("Nano initializing...");
 }
 
 void loop() {
 
-  if(HC12.available()) {
-    char letter = HC12.read();
+  if(Nano.available()) {
+    char letter = Nano.read();
     if(letter == '\n') {
       message += '\n';
       Serial.println(message);
+      if(message == "OFF\n") {
+        digitalWrite(redLed, LOW);
+      } else if(message == "ON\n") {
+        digitalWrite(redLed, HIGH);
+      }
       message = "";
     } else {
       message += letter;
     }
   }
-
-  if(Serial.available()) {
-    char letter = Serial.read();
-    if(letter == '\n') {
-      message += '\n';
-      Serial.print("Sending: ");
-      Serial.println(message);
-      HC12.end();
-
-      Nano.listen();
-      Nano.print(message);
-      Nano.end();
-      HC12.listen();
-    } else {
-      message += letter;
-    }
-  }
-
-
 
 }
--- arduino/demo/nano-uno-hc12-listening/nano-uno-hc12-listening.ino
+++ arduino/demo/nano-uno-hc12-listening/nano-uno-hc12-listening.ino
@@ -3,8 +3,8 @@
 //Constants (buttons)
 const int txHc12 = A0; //green tx
 const int rxHc12 = A1; //blue received
-const int txNano = 9; //green tx
-const int rxNano = 8; //blue received
+const int txNano = A2; //green tx
+const int rxNano = A3; //blue received
 const int waitingTime = 5000;
 
 bool isListeningToHc12 = true;
