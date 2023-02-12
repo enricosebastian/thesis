f012beb20f753e84ba25a9372050dc6cc03f127f
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2023-02-12 14:58:48 +0800

    added new demo for compass with hc12

---
+++ arduino/demo/uno-nano-compass-receiver/uno-nano-compass-receiver.ino
@@ -0,0 +1,45 @@
+#include <NeoSWSerial.h>
+
+//Constants (buttons)
+const int txHc12 = A0; //green tx
+const int rxHc12 = A1; //blue received
+
+String message = "";
+
+NeoSWSerial HC12(txHc12, rxHc12); // (Green TX, Blue RX)
+
+void setup() {
+  Serial.begin(9600);
+  HC12.begin(9600);
+  HC12.listen();
+  Serial.println("Initializing...");
+}
+
+void loop() {
+
+  if(HC12.available()) {
+    char letter = HC12.read();
+    if(letter == '\n') {
+      message += '\n';
+      Serial.println(message);
+      message = "";
+    } else {
+      message += letter;
+    }
+  }
+
+  if(Serial.available()) {
+    char letter = Serial.read();
+    if(letter == '\n') {
+      message += '\n';
+      Serial.print("Sending: ");
+      Serial.println(message);
+      HC12.print(message);
+    } else {
+      message += letter;
+    }
+  }
+
+
+
+}
