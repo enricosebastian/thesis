8b82502b13c6db868c5e2b874f433ce8a1bfd3c9
unknown <enrico_sebastian_salazar@dlsu.edu.ph>
2023-02-12 15:29:04 +0800

    added heading print functionality

--- arduino-nano/demo/uno-nano-compass-nanover/uno-nano-compass-nanover.ino
+++ arduino-nano/demo/uno-nano-compass-nanover/uno-nano-compass-nanover.ino
@@ -10,12 +10,20 @@ const int rxHc12 = 3; //blue received
 String message = "";
 
 NeoSWSerial HC12(txHc12, rxHc12); // (Green TX, Blue RX)
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 
 void setup() {
   Serial.begin(9600);
   HC12.begin(9600);
   HC12.listen();
   Serial.println("Initializing nano...");
+
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while(1);
+  }
 }
 
 void loop() {
@@ -31,19 +39,9 @@ void loop() {
     }
   }
 
-  if(Serial.available()) {
-    char letter = Serial.read();
-    if(letter == '\n') {
-      message += '\n';
-      Serial.print("Sending: ");
-      Serial.print(message);
-      HC12.print(message);
-      message = "";
-    } else {
-      message += letter;
-    }
-  }
-
-
-
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+  Serial.println(heading);
+  HC12.println(heading); 
 }
