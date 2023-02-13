82f3b05a02efe5b341187206e1cea1514a09592d
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2023-02-13 11:13:42 +0800

    fixed detection logic

--- arduino/prototype/main-14-new-compass-integration/main-14-new-compass-integration.ino
+++ arduino/prototype/main-14-new-compass-integration/main-14-new-compass-integration.ino
@@ -1,3 +1,4 @@
+// Ubuntu command line for serial port: sudo chmod a+rw /dev/ttyACM0
 #include <Arduino.h>
 #include <NeoSWSerial.h>
 #include <LinkedList.h>
@@ -333,15 +334,6 @@ void forDrone() {
         digitalWrite(yellowLed, LOW);
         digitalWrite(greenLed, HIGH);
         digitalWrite(detectionPin, HIGH);
-      } else if(receivedCommand == "DETE") {
-        hasDetectedObject = true;
-        if(receivedDetails == "DONE") {
-          hasDetectedObject = false;
-        }
-        digitalWrite(redLed, LOW);
-        digitalWrite(yellowLed, LOW);
-        digitalWrite(greenLed, HIGH);
-        sendToNano(receivedCommand, myName, receivedDetails);
       } else {
         sendToNano(receivedCommand, myName, receivedDetails);
       }
@@ -367,7 +359,11 @@ void forDrone() {
   
         receivedMessage = ""; // Erase old message
         if(receivedCommand == "DETE") {
+          hasDetectedObject = true;
           sendToNano(receivedCommand, myName, receivedDetails);
+          if(receivedDetails == "DONE") {
+            hasDetectedObject = false;
+          }
         }
       } else {
         receivedMessage += letter;
