7e22071cd75433e398708b5c5235f558419ffb5a
unknown <enrico_sebastian_salazar@dlsu.edu.ph>
2023-02-14 16:41:59 +0800

    working compass sensor

--- arduino-nano/demo/magsensor-x-y-demo/magsensor-x-y-demo.ino
+++ arduino-nano/demo/magsensor-x-y-demo/magsensor-x-y-demo.ino
@@ -55,9 +55,23 @@ void loop(void)
   /* Get a new sensor event */ 
   sensors_event_t event; 
   mag.getEvent(&event);
-  Serial.print(event.magnetic.x);
-  Serial.print("    ");
-  Serial.print(event.magnetic.y);
-  Serial.print("    ");
-  Serial.println(event.magnetic.z);
+  // Serial.print(event.magnetic.x);
+  // Serial.print("    ");
+  // Serial.print(event.magnetic.y);
+  // Serial.print("    ");
+  // Serial.println(event.magnetic.z);
+
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+
+  // Correct for when signs are reversed.
+  if(heading < 0)
+    heading += 2*PI;
+    
+  // Check for wrap due to addition of declination.
+  if(heading > 2*PI)
+    heading -= 2*PI;
+   
+  // Convert radians to degrees for readability.
+  float headingDegrees = heading * 180/M_PI; 
+  Serial.print("Heading (degrees): "); Serial.println(headingDegrees);
 }
