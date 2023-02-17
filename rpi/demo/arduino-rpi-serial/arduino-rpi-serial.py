eec5c3343e75e01aead7f2d8458eb526ef50d3d9
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2023-02-17 14:38:23 +0800

    fixed demo

--- rpi/demo/arduino-rpi-serial/arduino-rpi-serial.py
+++ rpi/demo/arduino-rpi-serial/arduino-rpi-serial.py
@@ -4,5 +4,7 @@ import time
 ser = serial.Serial("/dev/ttyS0", 9600)
 
 while True:
-    ser.write("Hello from Rpi\n".encode('utf-8'))
+    message = "DETE ALL ALL LEFT\n"
+    Serial.println("Sending", message)
+    ser.write(message.encode('utf-8'))
     time.sleep(1)
