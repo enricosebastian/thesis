0d05ccceb0f937cda2d0d45de8ace41bbdb2bb2e
enricosebastian <enricosebastiansalazar@gmail.com>
2023-02-19 17:02:28 +0800

    modified video recording file

--- rpi/demo/video-recording/video-recording.py
+++ rpi/demo/video-recording/video-recording.py
@@ -1,25 +1,49 @@
 import RPi.GPIO as GPIO
-
 import cv2
-from cv2 import VideoWriter
-from cv2 import VideoWriter_fourcc
 
-webcam = cv2.VideoCapture(0)
-video = VideoWriter('webcam.avi', VideoWriter_fourcc(*'MP42'), 24.0, (640, 480))
+cap = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+writer = cv2.VideoWriter("recording.mp4", fourcc, 24.0, (640, 480))
+
+recording = False
 
 while True:
-    stream_ok, frame = webcam.read()
-    
-    if stream_ok:
-      cv2.imshow('Webcam rn', frame)
-      video.write(frame)
-    
-    if cv2.waitKey(0) and 0xFF == ord('q'):
-        break
-
+   ret, frame = cap.read()
+   
+   if ret:
+      cv2.imshow('Video recording', frame)
+      if recording:
+         writer.write(frame)
+         
+   key = cv2.waitKey(1)
+   
+   if key == ord('q'):
+      break
+   elif key == ord('r'):
+      recording = not recording
+      print(f"Recaording: {recording}")
+      
+cap.release()
+writer.release()
 cv2.destroyAllWindows()
-webcam.release()
-video.release()
+
+# webcam = cv2.VideoCapture(0)
+# video = VideoWriter('webcam.avi', VideoWriter_fourcc(*'MP42'), 24.0, (640, 480))
+
+# while True:
+#     stream_ok, frame = webcam.read()
+    
+#     if stream_ok:
+#       cv2.imshow('Webcam rn', frame)
+#       video.write(frame)
+    
+#     if cv2.waitKey(0) and 0xFF == ord('q'):
+#         break
+
+# cv2.destroyAllWindows()
+# webcam.release()
+# video.release()
 
 # offPin = 11
 
