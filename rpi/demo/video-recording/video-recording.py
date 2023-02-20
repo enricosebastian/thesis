5f6711dc8d74b29a30ebe1910da2b8c115b3e86f
enricosebastian <enricosebastiansalazar@gmail.com>
2023-02-20 09:52:31 +0800

    initialized datetime test

--- rpi/demo/video-recording/video-recording.py
+++ rpi/demo/video-recording/video-recording.py
@@ -1,59 +1,31 @@
 import RPi.GPIO as GPIO
 import cv2
+import datetime
 
-cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture(0)
 
-fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-writer = cv2.VideoWriter("recording.mp4", fourcc, 24.0, (640, 480))
+# fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+# writer = cv2.VideoWriter("recording.mp4", fourcc, 24.0, (640, 480))
 
 recording = False
 
 while True:
-   ret, frame = cap.read()
+   print(datetime.datetime().microsecond())
+#    ret, frame = cap.read()
    
-   if ret:
-      cv2.imshow('Video recording', frame)
-      if recording:
-         writer.write(frame)
+#    if ret:
+#       cv2.imshow('Video recording', frame)
+#       if recording:
+#          writer.write(frame)
          
-   key = cv2.waitKey(1)
+#    key = cv2.waitKey(1)
    
-   if key == ord('q'):
-      break
-   elif key == ord('r'):
-      recording = not recording
-      print(f"Recaording: {recording}")
+#    if key == ord('q'):
+#       break
+#    elif key == ord('r'):
+#       recording = not recording
+#       print(f"Recaording: {recording}")
       
-cap.release()
-writer.release()
-cv2.destroyAllWindows()
-
-# webcam = cv2.VideoCapture(0)
-# video = VideoWriter('webcam.avi', VideoWriter_fourcc(*'MP42'), 24.0, (640, 480))
-
-# while True:
-#     stream_ok, frame = webcam.read()
-    
-#     if stream_ok:
-#       cv2.imshow('Webcam rn', frame)
-#       video.write(frame)
-    
-#     if cv2.waitKey(0) and 0xFF == ord('q'):
-#         break
-
+# cap.release()
+# writer.release()
 # cv2.destroyAllWindows()
-# webcam.release()
-# video.release()
-
-# offPin = 11
-
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(offPin, GPIO.IN)
-
-# while True:
-#     if GPIO.input(offPin) == True:
-#         print("on")
-#     else:
-#         print("off")
-
-
