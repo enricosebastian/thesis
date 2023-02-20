30945c04efd008a10bcd26093f191120a70bfed6
enricosebastian <enricosebastiansalazar@gmail.com>
2023-02-20 10:07:49 +0800

    working video recording script demo

--- rpi/demo/video-recording/video-recording.py
+++ rpi/demo/video-recording/video-recording.py
@@ -1,33 +1,35 @@
 import RPi.GPIO as GPIO
 import cv2
 import datetime
+import os
 
-# cap = cv2.VideoCapture(0)
+now = datetime.datetime.now()
+now_string = now.strftime("%H-%M-%S")
+filename = now_string+".mp4"
 
-# fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
-# writer = cv2.VideoWriter("recording.mp4", fourcc, 24.0, (640, 480))
+cap = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+writer = cv2.VideoWriter(filename, fourcc, 24.0, (640, 480))
 
 recording = False
 
 while True:
-   now = datetime.datetime.now()
-   now_string = now.strftime("%d/%m/%Y %H:%M:%S")
-   print(now_string)
-#    ret, frame = cap.read()
+   ret, frame = cap.read()
    
-#    if ret:
-#       cv2.imshow('Video recording', frame)
-#       if recording:
-#          writer.write(frame)
+   if ret:
+      cv2.imshow('Video recording', frame)
+      if recording:
+         writer.write(frame)
          
-#    key = cv2.waitKey(1)
+   key = cv2.waitKey(1)
    
-#    if key == ord('q'):
-#       break
-#    elif key == ord('r'):
-#       recording = not recording
-#       print(f"Recaording: {recording}")
+   if key == ord('q'):
+      break
+   elif key == ord('r'):
+      recording = not recording
+      print(f"Recording: {recording}")
       
-# cap.release()
-# writer.release()
-# cv2.destroyAllWindows()
+cap.release()
+writer.release()
+cv2.destroyAllWindows()
