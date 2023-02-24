d89a06e89e3b74d27706e29b6c28ff3e1254b673
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2023-02-24 15:01:04 +0800

    removed unnecessary files

--- to-delete/demo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-. "/home/pi/Desktop/tf/bin/activate" #activate the python virtual environment
-cd /home/pi/Desktop/thesis/python
-
-xterm -e python /home/pi/Desktop/thesis/python/demo.py &
-xterm -e python /home/pi/Desktop/thesis/python/sending_json_demo.py
--- to-delete/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. "/home/pi/Desktop/tf/bin/activate" #activate the python virtual environment
-cd /home/pi/Desktop/thesis/python
-
-lxterminal -e python /home/pi/Desktop/thesis/python/initial.py
-lxterminal -e python /home/pi/Desktop/thesis/python/reader.py
-lxterminal -e python /home/pi/Desktop/thesis/python/detect.py
