820983ddbfcca8e906d9759e52ac9d0cf8df9ec4
enricosebastian <enricosebastiansalazar@gmail.com>
2023-03-05 23:28:19 +0800

    added simulations with modifiable parameters

--- simulations/exhaustive-maneuvering.py
+++ simulations/exhaustive-maneuvering.py
@@ -2,20 +2,32 @@ import matplotlib.pyplot as plt
 import time
 import numpy as np
 
-num_drones = 3 # number of drones
+
+# Number of drones to deploy
+num_drones = 3 
+
+# You have to match the amount of colors to the number of drones
 colors = ["red", 
           "blue", 
           "green",
           ]
 
+# How fast/slow the coordinates will update
+draw_delay = 0.01
+
+# How big of small your plot image is
+x_lim = (-5,20)
+y_lim = (0, 20)
+
+
+# DEPLOYMENT PARAMETERS
+# How big or small your deployment zone is
 x_max = 20
 x_min = 0
 
+# How far or near your drones reach
 y_max = 20
 y_min = 0
-
-draw_delay = 0.01
-
 
 x = list(range(x_min, x_max, abs(x_max - x_min)//num_drones))
 x_max = x[0:len(x)-1]
@@ -45,8 +57,8 @@ while True:
             x_max[idx] = temp
     
     plt.scatter(x_data, y_data, c=colors)
-    plt.xlim(-20,20)
-    plt.ylim(0,20)
+    plt.xlim(x_lim)
+    plt.ylim(y_lim)
     
     plt.draw()
     
