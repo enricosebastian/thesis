92ef9b49ed632bf38245f934ec18fe1fad3fc802
unknown <enrico_sebastian_salazar@dlsu.edu.ph>
2023-03-07 18:01:39 +0800

    integrated new timer move in tag 03

--- esp32/prototype/tag-03/main-04-change-on-timer-improved/main-04-change-on-timer-improved.ino
+++ esp32/prototype/tag-03/main-04-change-on-timer-improved/main-04-change-on-timer-improved.ino
@@ -2,7 +2,7 @@
 #include "DW1000Ranging.h"
 #include "DW1000.h"
 
-#define TAG_ADD "01:20:5B:D5:A9:9A:E2:9C" // Tag 1
+#define TAG_ADD "03:20:5B:D5:A9:9A:E2:9C" // Tag 1
 
 #define TX 16
 #define RX 17
@@ -19,12 +19,12 @@ String message = "";
 
 String tempChannel = "";
 
-// tag 2
-String channel1 = "5"; // drone 1
+// tag 3
+String channel1 = "1"; // drone 3
 String channel2 = "1"; // none
-String channel3 = "7"; // drone 2
+String channel3 = "5"; // drone 1
 String channel4 = "1"; // none
-String channel5 = "1"; // drone 3
+String channel5 = "7"; // drone 2
 String channel6 = "1"; // none
 
 float t1 = 0.0;
@@ -44,7 +44,7 @@ void setup() {
   DW1000Ranging.attachNewDevice(0);
   DW1000Ranging.attachInactiveDevice(0);
 
-  Serial.println("Starting Tag 1");
+  Serial.println("Starting Tag 3");
 
   DW1000Ranging.startAsTag(TAG_ADD,DW1000.MODE_LONGDATA_RANGE_LOWPOWER,DW1000.CHANNEL_5,false);
 }
