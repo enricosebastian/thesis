d89b2e0dedccc92c186e3bf14a97c456eb8f1099
enricosebastian <enricosebastiansalazar@gmail.com>
2023-03-13 23:01:44 +0800

    simulated actual movement algorithm

--- simulations/exhaustive-maneuvering.py
+++ simulations/exhaustive-maneuvering.py
@@ -1,35 +1,42 @@
 import matplotlib.pyplot as plt
-import time
 import numpy as np
+import random
+import time
 
 
 # Number of drones to deploy
-num_drones = 3 
+num_drones = 3
 
 # You have to match the amount of colors to the number of drones
 colors = ["red", 
-          "blue", 
           "green",
-          ]
+          "blue",
+          "yellow",
+          "purple",
+          "orange",
+          "pink",]
+
+colors = colors[0:num_drones]
 
 # How fast/slow the coordinates will update
-draw_delay = 0.01
-
-# How big of small your plot image is
-x_lim = (-5,20)
-y_lim = (0, 20)
-
+draw_delay = 0.05
 
 # DEPLOYMENT PARAMETERS
 # How big or small your deployment zone is
-x_max = 20
+x_max = 17
 x_min = 0
 
 # How far or near your drones reach
-y_max = 20
+y_max = 18
 y_min = 0
 
-x = list(range(x_min, x_max, abs(x_max - x_min)//num_drones))
+# How big of small your plot image is
+x_lim = (x_min-5,x_max+5)
+y_lim = (y_min, y_max)
+
+# Important x points
+sizeOfArea = abs(x_max - x_min) // num_drones
+x = list(range(x_min, x_max+1, sizeOfArea))
 x_max = x[0:len(x)-1]
 x_min = x[1:len(x)]
 x_increment = np.ones(num_drones).astype(int) * -1
@@ -39,23 +46,26 @@ y_max = np.ones(num_drones).astype(int) * y_max #<--- edit number constant
 y_min = np.zeros(num_drones).astype(int)
 y_increment = np.ones(num_drones).astype(int) * 1
 y_data = y_min.copy()
-y_data = [1, 2, 0]
+y_data = random.sample(range(0,3), num_drones)
 
 while True:
     for idx, y in enumerate(y_data):
-        if y == y_max[idx]:
+        if y == y_max[idx] or y == y_min[idx]:
             y_increment[idx] *= -1
-            temp = y_min[idx]
-            y_min[idx] = y_max[idx]
-            y_max[idx] = temp
             x_data[idx] += x_increment[idx]
-            
+    
     for idx, x in enumerate(x_data):
         if x == x_max[idx]:
+            y_increment[idx] = 0
             x_increment[idx] *= -1
-            temp = x_min[idx]
-            x_min[idx] = x_max[idx]
-            x_max[idx] = temp
+        if x == x_min[idx]:
+            if y_data[idx] >= y_max[idx]:
+                y_increment[idx] = -1
+                x_increment[idx] = 0
+            elif y_data[idx] <= y_min[idx]:
+                y_increment[idx] = 1
+                x_increment[idx] = -1
+            
     
     plt.scatter(x_data, y_data, c=colors)
     plt.xlim(x_lim)
