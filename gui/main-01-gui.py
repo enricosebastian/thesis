223c601209538c42423b3ea2771f112f213966e3
unknown <enrico_sebastian_salazar@dlsu.edu.ph>
2023-03-16 12:29:48 +0800

    added serial port search functionality

--- gui/main-01-gui.py
+++ gui/main-01-gui.py
@@ -30,22 +30,34 @@ drone_counter.grid(column=0, row=1, columnspan=6, sticky="ew")
 ################
 
 # For port label
-ports = serial.tools.list_ports.comports()
+found_ports = serial.tools.list_ports.comports()
 
-select_port_label = Label(root, text="Select an Arduino COM port: ", width=50)
-select_port_label.grid(column=0, row=2, sticky="ew", columnspan=2)
+ports_dropdown_label = Label(root, text="Select an Arduino COM port: ", width=50)
+ports_dropdown_label.grid(column=0, row=2, sticky="ew", columnspan=2)
 #######################
 
+selected_port = 0
+serial_instance = serial.Serial()
+
 # For port drop down list
-port_list = ["N/A"]
+def set_port(port_name):
+    selected_port = port_name[0:4]
+    print("Selected port is:",selected_port)
+    serial_instance.port = selected_port
+    serial_instance.baudrate = 9600
+    serial_instance.open()
+    
+    
+    
+ports = ["N/A"]
 
-for port in ports:
-    port_list.append(str(port))
+for found_port in found_ports:
+    ports.append(str(found_port))
 
-selected_port = StringVar(root)
-selected_port.set(port_list[0])
+port_list = StringVar(root)
+port_list.set(ports[0])
 
-port_dropdown_list = ttk.OptionMenu(root, selected_port, *port_list)
+port_dropdown_list = ttk.OptionMenu(root, port_list, *ports, command=set_port)
 port_dropdown_list.config(width=50)
 port_dropdown_list.grid(column=3, row=2, sticky="ew", columnspan=3)
 #################################
@@ -54,7 +66,7 @@ port_dropdown_list.grid(column=3, row=2, sticky="ew", columnspan=3)
 drones = serial.tools.list_ports.comports()
 
 select_drone_label = Label(root, text="Select drone: ", width=50)
-select_drone_label.grid(column=0, row=2, sticky="ew", columnspan=2)
+select_drone_label.grid(column=0, row=3, sticky="ew", columnspan=2)
 #######################
 
 # For drone selection list
@@ -69,70 +81,75 @@ selected_drone.set(drone_list[0])
 
 select_drone_dropdown_list = ttk.OptionMenu(root, selected_drone, *drone_list)
 select_drone_dropdown_list.config(width=50)
-select_drone_dropdown_list.grid(column=3, row=2, sticky="ew", columnspan=3)
+select_drone_dropdown_list.grid(column=3, row=3, sticky="ew", columnspan=3)
 #################################
 
 # For deploy button
 deploy_button = Button(root, text="Deploy drone")
-deploy_button.grid(column=0, row=3, sticky="ew", padx=5, pady=5)
+deploy_button.grid(column=0, row=4, sticky="ew", padx=5, pady=5)
 ##################
 
 # For home button
 home_button = Button(root, text="Send drone home")
-home_button.grid(column=1, row=3, sticky="ew", padx=5, pady=5)
+home_button.grid(column=1, row=4, sticky="ew", padx=5, pady=5)
 ##################
 
 # For stop button
 stop_button = Button(root, text="Stop drone")
-stop_button.grid(column=2, row=3, sticky="ew", padx=5, pady=5)
+stop_button.grid(column=2, row=4, sticky="ew", padx=5, pady=5)
 ##################
 
 # For where button
 location_button = Button(root, text="Ask location of drone")
-location_button.grid(column=3, row=3, sticky="ew", padx=5, pady=5)
+location_button.grid(column=3, row=4, sticky="ew", padx=5, pady=5)
 ##################
 
 # For cancel command button
 cancel_command_button = Button(root, text="Stop sending command")
-cancel_command_button.grid(column=4, row=3, sticky="ew", padx=5, pady=5)
+cancel_command_button.grid(column=4, row=4, sticky="ew", padx=5, pady=5)
 ##################
 
 # For region of responsibility label
 drones = serial.tools.list_ports.comports()
 
 select_drone_label = Label(root, text="Deployment area", width=50)
-select_drone_label.grid(column=0, row=4, sticky="ew", columnspan=5)
+select_drone_label.grid(column=0, row=5, sticky="ew", columnspan=5)
 #######################
 
 # For y_max
 y_max_label = Label(root, text="Y max", width=50)
-y_max_label.grid(column=0, row=5, sticky="ew", columnspan=2)
+y_max_label.grid(column=0, row=6, sticky="ew", columnspan=2)
 
 y_max_input_label = ttk.Entry()
-y_max_input_label.grid(column=3, row=5, sticky="ew", columnspan=2)
+y_max_input_label.grid(column=3, row=6, sticky="ew", columnspan=2)
 #################################
 
 # For y_max
 y_min_label = Label(root, text="Y min", width=50)
-y_min_label.grid(column=0, row=6, sticky="ew", columnspan=2)
+y_min_label.grid(column=0, row=7, sticky="ew", columnspan=2)
 
 y_min_input_label = ttk.Entry()
-y_min_input_label.grid(column=3, row=6, sticky="ew", columnspan=2)
+y_min_input_label.grid(column=3, row=7, sticky="ew", columnspan=2)
 #################################
 
 # For anchor distance
 anchor_distance_label = Label(root, text="Anchor distance", width=50)
-anchor_distance_label.grid(column=0, row=7, sticky="ew", columnspan=2)
+anchor_distance_label.grid(column=0, row=8, sticky="ew", columnspan=2)
 
 anchor_distance_input_label = ttk.Entry()
-anchor_distance_input_label.grid(column=3, row=7, sticky="ew", columnspan=2)
+anchor_distance_input_label.grid(column=3, row=8, sticky="ew", columnspan=2)
 #################################
 
 # For serial log terminal
 serial_terminal = Text(root, width=50)
-serial_terminal.insert(INSERT, "HELLO")
 
-serial_terminal.grid(column=0, row=8, sticky="ew", columnspan=5)
+while selected_port != 0:
+    if serial_instance.in_waiting:
+        character = serian_instance.readline()
+        print(character.decode('utf'))
+        # serial_terminal.insert("1.0", "HELLO "+str(i)+"\n")
+
+serial_terminal.grid(column=0, row=9, sticky="ew", columnspan=5)
 #################################
 
 root.mainloop()
