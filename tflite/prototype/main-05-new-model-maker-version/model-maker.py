f1e46071e38de1e84619948a38b7f8c727df2e39
enricosebastian <enrico_sebastian_salazar@dlsu.edu.ph>
2023-03-26 22:45:45 +0800

    generated new dataset model

--- tflite/prototype/main-05-new-model-maker-version/model-maker.py
+++ tflite/prototype/main-05-new-model-maker-version/model-maker.py
@@ -19,8 +19,8 @@ from absl import logging
 logging.set_verbosity(logging.ERROR)
 
 # CHANGE VALUES HERE ONLY
-batch_size = 2 # default is 8
-epochs = 100 # default is 50
+batch_size = 8 # default is 8
+epochs = 50 # default is 50
 
 model_name = 'efficientdet_lite0'
 train_dir = './images/all/train'
